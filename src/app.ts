9fade76a74db4b2e3d89ace756c359637621fdcb
Eric Eichhorn <ee.eric.eichhorn@outlook.de>
2018-10-31 14:00:40 +0100

    added icon

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Animation } from "./enc/animation";
 import { MainMenuView } from "./mainMenuView";
 import { GameView } from "./gameView";
-
+import favicon from "./assets/favicon.png"
 
 export class App {
     animation: Animation;
@@ -10,20 +10,33 @@ export class App {
 
     public run() {
         this.animation = Animation.createInBody();
+        this.changeFavicon(favicon)
         this.newGame();
     }
-    
+
+    changeFavicon(src: string) {
+        var link = document.createElement('link'),
+            oldLink = document.getElementById('dynamic-favicon');
+        link.id = 'dynamic-favicon';
+        link.rel = 'shortcut icon';
+        link.href = src;
+        if (oldLink) {
+            document.head.removeChild(oldLink);
+        }
+        document.head.appendChild(link);
+    }
+
     private start = () => {
         this.game = new GameView(this.menu.controllers);
         this.menu.hide();
         this.menu.requestStart.removeEventListener(this.start);
         this.menu = null;
-        
+
         this.animation.addView(this.game);
         this.game.requestNewGame.addEventListener(this.newGame);
         this.game.start();
     }
-    
+
     private newGame = () => {
         this.menu = new MainMenuView();
         this.menu.requestStart.addEventListener(this.start);
@@ -32,7 +45,7 @@ export class App {
             this.game.hide();
             this.game = null;
         }
-        
+
         this.animation.addView(this.menu);
         this.menu.show();
     }
